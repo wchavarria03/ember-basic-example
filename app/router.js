09e88d1fe7797a73c8196068984b91ea0e685dd9
wchavarria-as <wchavarria-as@sca-ws286.avantek-sc.avantica.avanticatec.net>
2017-08-21 17:58:00 -0600

    Updating styles

--- app/router.js
+++ app/router.js
@@ -7,10 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('app', function() {
-    this.route('index', { path: ''},function() {
+  this.route('app', { path: '/'}, function() {
+    this.route('index', { path: '/'},function() {
       this.route('settings', { path: 'settings'});
-      this.route('reports', { path: 'reports'});
+      this.route('reports', { path: '/'});
     });
   });
 });
