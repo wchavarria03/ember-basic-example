2297ae3c4feee3c7ae980316746d02faa8491362
Walter Chavarria <wchavarria03@gmail.com>
2017-09-02 18:54:40 -0600

    Adding settings mobile view

--- app/components/layout-footer/component.js
+++ app/components/layout-footer/component.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNameBindings: ['inSettings'],
+  inSettings: function() {
+    return this.get('inSettings');
+  },
 });
